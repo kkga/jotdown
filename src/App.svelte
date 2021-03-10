d1b4e636ed73d623acc6d7464d83fca6ef659927
Gadzhi Kharkharov <me@kkga.me>
2021-03-10 13:17:04 +0300

    upd

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
   import Pad from "./components/Pad.svelte";
-  import Alert from "./components/Alert.svelte";
 
   import { sheets } from "./stores";
 </script>
 
-<!--<Alert /> -->
 <Pad bind:sheets={$sheets} />
